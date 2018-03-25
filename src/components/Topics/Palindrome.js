33d6cd18dc7a14c33c9776c1ba40266b1a53e212
Paul Peterson <paulpeterson@Pauls-MacBook-Pro.local>
2018-03-24 21:22:48 -0600

    initial

---
+++ src/components/Topics/Palindrome.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Palindrome extends Component {
+    render() {
+        return (
+            <div>
+                <p>Palindrome Component</p>
+            </div>
+        )
+    }
+}
